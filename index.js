332ea305b1919874b543b561fa0c3c83f1f00e24
mogwai <feelirabs@gmail.com>
2020-10-22 00:35:35 +0100

    animations entering

---
+++ index.js
@@ -0,0 +1,7 @@
+function addLoader(){
+    document.getElementById("h1").style.marginTop = "0em";
+    document.getElementById("checklist").style.opacity = "1";
+    document.getElementById("scroll").style.opacity = "1";
+}
+
+window.onload = addLoader();
