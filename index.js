b6eb430abc11004e81f68c6d14aefb74d351b478
mogwai <feelirabs@gmail.com>
2020-10-25 03:50:17 +0100

    fix wrap problem

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 function addLoader(){
     document.getElementById("h1").style.marginTop = "0em";
-    document.getElementById("checklist").style.opacity = "1";
     document.getElementById("scroll").style.opacity = "1";
 }
 
